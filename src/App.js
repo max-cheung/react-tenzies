65e090f998406ef58ece52958631f6744fab7284
Max Cheung <maxcheung07@gmail.com>
2023-12-13 18:24:58 -0800

    changed dice to objects and added nanoid as dependency

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Die from "./Die"
+import {nanoid} from "nanoid"
 
 function App() {
     const [dice, setDice] = React.useState(allNewDice())
@@ -7,21 +8,33 @@ function App() {
     function allNewDice() {
         const arr = [];
         for(let i=0; i<10; i++) {
-            arr.push(Math.ceil(Math.random()*6))
+            const num = Math.ceil(Math.random()*6);
+            arr.push({
+                id: nanoid(),
+                value: num,
+                isHeld: false
+            })
         }
 
         return arr;
     }
 
+    function rollDice() {
+        setDice(allNewDice);
+    }
+
     const diceElements = dice.map(die => {
-        return <Die value={die} />
+        return <Die key={die.id} value={die.value} />
     })
+
+    console.log(dice);
 
     return (
         <main>
             <div className="container">
                 {diceElements}
             </div>
+            <button onClick={rollDice} className="roll-dice">Roll</button>
         </main>
     );
 }
